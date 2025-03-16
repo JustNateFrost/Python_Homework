a035eab34490d1d2511d524571f61c3075ed8ba9
JustNateFrost <natalie.1989.tupitsina@gmail.com>
2025-03-16 18:55:03 +0500

    после flake8

--- Lesson_05/modal_window.py
+++ Lesson_05/modal_window.py
@@ -7,7 +7,8 @@ driver = webdriver.Firefox()
 
 driver.get("http://the-internet.herokuapp.com/entry_ad")
 sleep(2)
-element = driver.find_element(By.CSS_SELECTOR, ".modal-footer > p:nth-child(1)")
+element = driver.find_element(
+    By.CSS_SELECTOR, ".modal-footer > p:nth-child(1)")
 element.click()
 sleep(2)
 driver.quit()
