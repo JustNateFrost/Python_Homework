304e96498f48b546027f680f2ac7ea5716275a2f
JustNateFrost <natalie.1989.tupitsina@gmail.com>
2025-03-28 20:18:30 +0500

    После flake8

--- Lesson_07/CartPage.py
+++ Lesson_07/CartPage.py
@@ -12,12 +12,12 @@ class CartPage:
         backpack = self.driver.find_element(
             By.XPATH, '//*[@id="item_4_title_link"]/div').text
         return backpack
-    
+
     def check_shirt(self):
         shirt = self.driver.find_element(
             By.XPATH, '//*[@id="item_1_title_link"]/div').text
         return shirt
-    
+
     def check_onesie(self):
         onesie = self.driver.find_element(
             By.XPATH, '//*[@id="item_2_title_link"]/div').text
