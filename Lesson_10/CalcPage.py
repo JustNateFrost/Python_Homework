e58f9bbbad641c05f61a28f8f4e8cb8935b25e34
JustNateFrost <natalie.1989.tupitsina@gmail.com>
2025-04-20 18:07:24 +0500

    Задокументировала классы

--- Lesson_10/CalcPage.py
+++ Lesson_10/CalcPage.py
@@ -6,27 +6,43 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class CalcPage:
     def __init__(self, driver):
+        """
+            Эта функция инициализирует браузер
+        """
         self.driver = driver
         self.wait = WebDriverWait(driver, 45)
 
     @allure.step("Открыть сайт с калькулятором в браузере")
     def open(self):
+        """
+        Эта функция открывает сайт
+        """
         self.driver.get(
             "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
     @allure.step("Очистить и ввести в окно задержки значение 45")
     def delay(self):
+        """
+        Эта функция очищает и вводит значение 45 в окно для задержки
+        """
         self.driver.find_element(By.XPATH, '//*[@id="delay"]').clear()
         self.driver.find_element(By.XPATH, '//*[@id="delay"]').send_keys("45")
 
     @allure.step("Набрать в калькуляторе пример 7+8=")
     def num(self):
+        """
+        Эта функция набирает пример 7+8=
+        """
         self.driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[1]').click()
         self.driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[4]').click()
         self.driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[2]').click()
         self.driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[15]').click()
 
-    def wait_res(self):
+    def wait_res(self) -> str:
+        """
+        Эта функция ожидает результат
+        Возвращает текст полученного результата
+        """
         self.wait.until(
             EC.text_to_be_present_in_element((
                 By.XPATH, '//*[@id="calculator"]/div[1]/div'), "15"))
--- Lesson_10/OrderPage.py
+++ Lesson_10/OrderPage.py
@@ -6,21 +6,34 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class OrderPage:
     def __init__(self, driver):
+        """
+        Эта функция инициализирует браузер
+        """
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
     @allure.step("Перейти на сайт с оформлением заказа")
-    def open(self):
+    def open(self) -> None:
+        """
+        Эта функция открывает сайт
+        """
         self.driver.get("https://www.saucedemo.com/checkout-step-one.html")
 
     @allure.step("Вести персональные данные")
-    def data(self):
+    def data(self) -> None:
+        """
+        Эта функция вводит в поля имя, фамилию и индекс, нажимает на кнопку Continue
+        """
         self.driver.find_element(By.ID, "first-name").send_keys("Анна")
         self.driver.find_element(By.ID, "last-name").send_keys("Зайцева")
         self.driver.find_element(By.ID, "postal-code").send_keys("246801")
         self.driver.find_element(By.ID, "continue").click()
 
-    def total(self):
+    def total(self) -> str:
+        """
+        Эта функция ожидает итоговую стоимость покупки
+        Возвращает текст полученной стоимости
+        """
         self.wait.until(
             EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label"))
         )
